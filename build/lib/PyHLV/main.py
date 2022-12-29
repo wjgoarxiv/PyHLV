e9bf7b26356331411763e25059abb38298757384
wjgoarxiv <118244317+wjgoarxiv@users.noreply.github.com>
2022-12-29 14:03:17 +0900

    Create main.py

---
+++ build/lib/PyHLV/main.py
@@ -0,0 +1,7 @@
+def main(): 
+    print('PyHLV main.py')
+    print('PyHLV is a Python package for HLV analysis.')
+    print('Please visit https://pypi.org/project/PyHLV/ for more information.')
+
+if __name__ == '__main__':
+    main()
